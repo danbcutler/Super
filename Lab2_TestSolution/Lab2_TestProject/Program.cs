72faebedc78e6069d431b96389e2e3e37b87dfc2
danbcutler <dan.cutler1994@gmail.com>
2015-09-02 15:35:53 -0600

    Wrote the writeThis function.

--- Lab2_TestSolution/Lab2_TestProject/Program.cs
+++ Lab2_TestSolution/Lab2_TestProject/Program.cs
@@ -10,8 +10,20 @@ namespace Lab2_TestProject
     {
         static void Main(string[] args)
         {
+            String name = "Daniel";
+            String verb = "pooping";
+            String adjective = "laconically";
+
+            writeThis(name, verb, adjective);
             Console.Write("Hello World!");
             Console.Read();
         }
+        public static void writeThis(String name, String verb, String adjective)
+        {
+            String total = "Hello" + name + ".";
+            total += "I've been " + verb + " until we met. I'm so " + adjective + " for this moment.";
+            Console.Write(total);
+        }
+
     }
 }
